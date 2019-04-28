b87f981ad9b30ad91a8a74309a755828f2221c43
Sajay Antony <sajaya@microsoft.com>
2019-04-27 18:03:09 -0700

    Updated push to use oras bundling of directory
    Signed-off-by: Sajay Antony <sajaya@microsoft.com>

--- dotnet-start/EntryPointHandler.cs
+++ dotnet-start/EntryPointHandler.cs
@@ -13,25 +13,43 @@ public static class EntryPointHandler
     }
     static Manifest GetManifest(string appDirectory)
     {
-        var manifestFilePath = Path.Join(appDirectory, "manifest.json");
-        var manifestJson = File.ReadAllText(manifestFilePath);
-        var manifest = JsonConvert.DeserializeObject<Manifest>(manifestJson);
-        return manifest;
+        var entryFile = System.IO.Directory.GetFiles(
+                appDirectory,
+                "*.runtimeconfig.json");
+
+        if (entryFile.Length != 1)
+        {
+            var paths = string.Join(',', entryFile);
+            Console.WriteLine($"Found {entryFile.Length} entry point: {paths}");
+            Console.WriteLine("Unable to determine entry points.");
+            Environment.Exit(1);
+        }
+
+        var fileNameWithoutExtension = entryFile[0].Substring(0, entryFile[0].LastIndexOf(".runtimeconfig.json"));
+        var dllName = fileNameWithoutExtension + ".dll";
+
+        if (!File.Exists(dllName))
+        {
+            Console.WriteLine($"ERR: Unable to find {dllName}");
+        }
+
+        return new Manifest
+        {
+            entrypoint = dllName
+        };
     }
 
 
     // Handle console streaming and other things. 
     public static int RunProc(Manifest manifest, string appDirectory, string[] args)
     {
-        var entryPoint = Path.Join(Path.GetFullPath(appDirectory), manifest.entrypoint);
 
-        Console.WriteLine("| Starting application.");
-        Console.WriteLine("- " + Path.GetRelativePath(AppStore.Root, entryPoint));
+        Console.WriteLine("| Starting application");
         var psi = new ProcessStartInfo
         {
             FileName = "dotnet",
             WorkingDirectory = Environment.CurrentDirectory,
-            Arguments = String.Join(' ', entryPoint, args),
+            Arguments = String.Join(' ', manifest.entrypoint, args),
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             UseShellExecute = false
